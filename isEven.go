60a72f57795a84d2bb3a7bc8e6fc37fd865ee8a9
AvicennaJr <justfuad@protonmail.com>
2023-03-05 22:02:13 +0300

    Add main package

---
+++ isEven.go
@@ -0,0 +1,12 @@
+package iseven
+
+// This function will check if a number
+// is even. It will return true if true,
+// or false if not.
+func IsEven(n int) bool {
+	if (n % 2) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
